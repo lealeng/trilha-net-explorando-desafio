54c65bd625c81ef6754834326e5d63600b1e7581
lealeng <daniel.suportte@gmail.com>
2025-06-03 00:29:26 -0300

    desafio-concluido

--- Models/Reserva.cs
+++ Models/Reserva.cs
@@ -17,7 +17,8 @@ namespace DesafioProjetoHospedagem.Models
         {
             // TODO: Verificar se a capacidade é maior ou igual ao número de hóspedes sendo recebido
             // *IMPLEMENTE AQUI*
-            if (true)
+
+            if (Suite.Capacidade >= hospedes.Count)
             {
                 Hospedes = hospedes;
             }
@@ -25,6 +26,7 @@ namespace DesafioProjetoHospedagem.Models
             {
                 // TODO: Retornar uma exception caso a capacidade seja menor que o número de hóspedes recebido
                 // *IMPLEMENTE AQUI*
+                throw new Exception("Capacidade da suite é menor que o número de hóspedes.");
             }
         }
 
@@ -37,7 +39,16 @@ namespace DesafioProjetoHospedagem.Models
         {
             // TODO: Retorna a quantidade de hóspedes (propriedade Hospedes)
             // *IMPLEMENTE AQUI*
-            return 0;
+            int quantidadeHospedes = Hospedes.Count;
+
+            if (quantidadeHospedes > 0)
+            {
+                return quantidadeHospedes;
+            }
+            else
+            {
+                throw new Exception("Nenhum hóspede cadastrado.");
+            }
         }
 
         public decimal CalcularValorDiaria()
@@ -45,15 +56,25 @@ namespace DesafioProjetoHospedagem.Models
             // TODO: Retorna o valor da diária
             // Cálculo: DiasReservados X Suite.ValorDiaria
             // *IMPLEMENTE AQUI*
+
             decimal valor = 0;
+            if (Suite != null)
+            {
+                valor = DiasReservados * Suite.ValorDiaria;
+            }
+            if (DiasReservados >= 10)
+            {
+                // Regra: Caso os dias reservados forem maior ou igual a 10, conceder um desconto de 10%
+                valor -= valor * 0.1m; // Aplicando desconto de 10%
+            }
+            else if (Suite == null)
+            {
+                throw new Exception("Nenhuma suíte cadastrada.");
+            }
+
 
             // Regra: Caso os dias reservados forem maior ou igual a 10, conceder um desconto de 10%
             // *IMPLEMENTE AQUI*
-            if (true)
-            {
-                valor = 0;
-            }
-
             return valor;
         }
     }
--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
-﻿using System.Text;
+﻿using System.Security.Cryptography;
+using System.Text;
 using DesafioProjetoHospedagem.Models;
 
 Console.OutputEncoding = Encoding.UTF8;
@@ -6,17 +7,19 @@ Console.OutputEncoding = Encoding.UTF8;
 // Cria os modelos de hóspedes e cadastra na lista de hóspedes
 List<Pessoa> hospedes = new List<Pessoa>();
 
-Pessoa p1 = new Pessoa(nome: "Hóspede 1");
-Pessoa p2 = new Pessoa(nome: "Hóspede 2");
+Pessoa p1 = new Pessoa(nome: "WEX");
+Pessoa p2 = new Pessoa(nome: "Buta");
+Pessoa p3 = new Pessoa(nome: "DIO");
 
 hospedes.Add(p1);
 hospedes.Add(p2);
+hospedes.Add(p3);
 
 // Cria a suíte
-Suite suite = new Suite(tipoSuite: "Premium", capacidade: 2, valorDiaria: 30);
+Suite suite = new Suite(tipoSuite: "Premium", capacidade: 3, valorDiaria: 120);
 
 // Cria uma nova reserva, passando a suíte e os hóspedes
-Reserva reserva = new Reserva(diasReservados: 5);
+Reserva reserva = new Reserva(diasReservados: 10);
 reserva.CadastrarSuite(suite);
 reserva.CadastrarHospedes(hospedes);
 
